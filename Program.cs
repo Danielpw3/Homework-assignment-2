bc1dab23f3a7727b5dceeb6a2e36dd123f4e84ab
Daniel Parbst Waldau <DAST@simcorp.com>
2024-11-19 11:04:29 +0100

    Homework2 commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
 namespace Homework_Assignment_2
 {
-    internal class Program
+    class Transaction
+    {
+        internal enum TransactionType
+        {
+            Buy = -1,
+            Sell = 1
+        }
+    }
+    public class Program
     {
         static void Main(string[] args)
         {
+            // User inputs
+            Console.WriteLine("Enter transaction type (Buy/Sell):");
+            string transactionTypeInput = Console.ReadLine();
+
+            Console.WriteLine("Enter nominal value:");
+            var nominalInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter price:");
+            var priceInput = double.Parse(Console.ReadLine());
+
+            var priceOrgiginal = 100;
+
+            // Ternary operators
+            Transaction.TransactionType transactionType = transactionTypeInput.Equals("Buy")
+                ? Transaction.TransactionType.Buy
+                : Transaction.TransactionType.Sell;
+
+            var profitLoss = transactionTypeInput.Equals("Sell")
+                ? (priceInput - priceOrgiginal) * nominalInput
+                : 0;
+
+            // Value
+            var value = (int)transactionType * nominalInput * priceInput;
+           
+
+            // Output of the results
+            Console.WriteLine($"The value is: {value} DKK");
+            Console.WriteLine($"The profitLoss is: {profitLoss} DKK");
+
+            Console.ReadKey();
         }
     }
 }
